dce13663583fa967c022fb35a50a532b7cd6862a
Alok Raj <alokraaj04@gmail.com>
2026-04-22 22:07:06 +0530

    Test changed

--- backend/src/test/java/com/alok/Expense_Tracker/ExpenseTrackerApplicationTests.java
+++ backend/src/test/java/com/alok/Expense_Tracker/ExpenseTrackerApplicationTests.java
@@ -3,11 +3,10 @@ package com.alok.Expense_Tracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ExpenseTrackerApplication.class)
 class ExpenseTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
