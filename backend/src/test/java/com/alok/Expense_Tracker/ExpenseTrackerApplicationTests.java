e938529f0249e0c5a676b4fa38ec7274b904b3a5
Alok Raj <alokraaj04@gmail.com>
2026-04-22 22:17:41 +0530

    Test3

--- backend/src/test/java/com/alok/Expense_Tracker/ExpenseTrackerApplicationTests.java
+++ backend/src/test/java/com/alok/Expense_Tracker/ExpenseTrackerApplicationTests.java
@@ -1,9 +1,7 @@
 package com.alok.Expense_Tracker;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = ExpenseTrackerApplication.class)
 class ExpenseTrackerApplicationTests {
 
     @Test
